1b7afaad49b44db106fa54ea21d8183b0d1c274f
JustusKelsch <33078472+JustusKelsch@users.noreply.github.com>
2024-01-08 13:22:28 -0700

    Added a start screen and a button to open the main window. Added a checkbox for tracking results after the game is completed. No functionality yet.

---
+++ WPFBingoCaller/StartScreen.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFBingoCaller
+{
+    /// <summary>
+    /// Interaction logic for StartScreen.xaml
+    /// </summary>
+    public partial class StartScreen : Window
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+            MainWindow mainWindow = new MainWindow();
+
+            this.Close();
+            mainWindow.Show();
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+
+            
+            
+        }
+    }
+}
